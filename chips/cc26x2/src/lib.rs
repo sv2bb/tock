bdd632af6768d609d08cf489ca004b9a4b487855
Brad Campbell <bradjc5@gmail.com>
2018-12-07 22:27:46 -0500

    cc2652 fixes

--- chips/cc26x2/src/lib.rs
+++ chips/cc26x2/src/lib.rs
@@ -2,13 +2,6 @@
 #![no_std]
 #![crate_name = "cc26x2"]
 #![crate_type = "rlib"]
-extern crate cortexm4;
-extern crate tock_rt0;
-#[allow(unused_imports)]
-#[macro_use]
-extern crate kernel;
-#[macro_use]
-extern crate enum_primitive;
 
 pub mod aon;
 pub mod chip;
